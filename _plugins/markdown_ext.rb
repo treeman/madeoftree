bb3b979fab9a0e54a7fbabe287143d9f0a2514cb
Jonas Hietala <tradet.h@gmail.com>
2012-08-10 13:27:00 +0200

    Remove comments etc.

--- _plugins/markdown_ext.rb
+++ _plugins/markdown_ext.rb
@@ -16,20 +16,7 @@ module Jekyll
                         frameborder="0">
                 </iframe>')
 
-            # This adds the gist #1 syntax.
-            # old_convert content.gsub(/gist #(\d+)/, '[gist #\1](https://gist.github.com/\1)')
         end
     end
 end
-
-            # Embed youtube links
-            #$content =~ s|
-            #    <p>\s*\Qhttp://www.youtube.com/watch?v=\E(\S+)\s*</p>
-            #|
-            #    <iframe class="youtube-player" type="text/html"
-            #            width="600" height="385"
-            #            src="http://www.youtube.com/embed/$1"
-            #            frameborder="0">
-            #    </iframe>
-            #|gx;
 
